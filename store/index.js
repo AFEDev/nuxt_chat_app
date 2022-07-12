5f3ffe1606b41cd7bc0c89493780c2c47b4250c0
AFEDev <algirduj@gmail.com>
2022-07-12 17:45:44 +0300

    user welcome messages, connection to chart room

--- store/index.js
+++ store/index.js
@@ -6,15 +6,14 @@ export const state = () => ({
 export const mutations = {
   setUser(state, user) {
     state.user = user;
+    console.log("store user suveike", user);
   },
   clearData(state) {
     state.user = {};
     state.messages = [];
   },
-};
-
-export const actions = {
-  newMessage(contex, data) {
-    console.log("Message received", data);
+  newMessage(state, message) {
+    state.messages.push(message);
+    console.log("message from store", message);
   },
 };
