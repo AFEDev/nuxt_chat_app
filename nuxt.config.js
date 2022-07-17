12a050d1c31f392db0641a089c8a4ee0856be6b2
AFEDev <algirduj@gmail.com>
2022-07-17 12:06:06 +0300

    firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 import colors from "vuetify/es5/util/colors";
 
 export default {
-  target: "static",
-  router: {
-    base: "/<repository-name>/",
-  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: "%s | Nuxt chat",
